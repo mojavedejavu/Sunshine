a1a76bf76c3d2a2ec43ede4d83be5ee137e94094
Xiaofan <ufa.fan@gmail.com>
2015-12-24 21:42:18 -0800

    parse JSON and format the date

--- app/src/main/java/com/example/xfang/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/xfang/sunshine/ForecastFragment.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,7 +48,7 @@ public class ForecastFragment extends Fragment {
 
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
                 R.layout.list_item_forecast, forecastData);
         listView.setAdapter(adapter);
 
@@ -73,24 +78,40 @@ public class ForecastFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String>{
+
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
 
         String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         @Override
-        protected String doInBackground(String... params){
+        protected String[] doInBackground(String... params){
             // declare these outside of the try block so they can be closed
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
+
+            String format = "json";
+            String unit = "metric";
+            int numDays = 7;
+
             try {
-                String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String API_PARAM = "APPID";
+
                 Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon().
-                        appendQueryParameter("q", params[0]).
-                        appendQueryParameter("units", "metric").
-                        appendQueryParameter("cnt","7").
-                        appendQueryParameter("APPID",getResources().getString(R.string.WeatherAPIKey)).build();
+                        appendQueryParameter(QUERY_PARAM, params[0]).
+                        appendQueryParameter(FORMAT_PARAM, format).
+                        appendQueryParameter(UNITS_PARAM, unit).
+                        appendQueryParameter(DAYS_PARAM, String.valueOf(numDays)).
+                        appendQueryParameter(API_PARAM,getResources().getString(R.string.WeatherAPIKey)).build();
 
                 URL url = new URL(builtUri.toString());
+                Log.d(LOG_TAG, "URL: " + url);
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
@@ -101,8 +122,10 @@ public class ForecastFragment extends Fragment {
                 while ((line = reader.readLine()) != null){
                     buffer.append(line + "\n");
                 }
+
                 Log.d(LOG_TAG, buffer.toString());
-                return buffer.toString();
+                String[] parsedForecasts = Utilities.getWeatherDataFromJson(buffer.toString(), numDays);
+                return parsedForecasts;
             }
             catch(IOException e){
                 Log.e(LOG_TAG, "Error ", e);
---
+++ app/src/main/java/com/example/xfang/sunshine/Utilities.java
@@ -0,0 +1,55 @@
+package com.example.xfang.sunshine;
+
+import android.text.format.Time;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by xfang on 12/24/15.
+ */
+public class Utilities {
+
+    private static String getReadableDate(long time){
+        SimpleDateFormat dayFormat = new SimpleDateFormat("EEE, MMM dd");
+        return dayFormat.format(time);
+    }
+
+    public static String[] getWeatherDataFromJson(String jsonString, int numDays) {
+        String LOG_TAG = "getWeatherDataFromJson";
+        String[] result = new String[numDays];
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray array = jsonObject.getJSONArray("list");
+
+            Time time = new Time();
+            time.setToNow();
+            int firstDayJulian = time.getJulianDay(time.toMillis(true), time.gmtoff);
+
+            for(int i = 0; i < array.length(); i++) {
+                JSONObject daily = (JSONObject) array.get(i);
+                JSONObject tempObject = daily.getJSONObject("temp");
+                String max = tempObject.getString("max");
+                String min = tempObject.getString("min");
+
+                JSONObject weatherObject = (JSONObject) daily.getJSONArray("weather").get(0);
+                String description = weatherObject.getString("main");
+
+                String day = getReadableDate(new Time().setJulianDay(firstDayJulian + i));
+
+                String dailyString = day + " " + min + " ~ " + max + ", " + description;
+                result[i] = dailyString;
+                Log.d(LOG_TAG, dailyString);
+            }
+
+        } catch (JSONException e) {
+            Log.d(LOG_TAG, "JSON parsing error: " + e);
+        }
+
+        return result;
+    }
+}
