79f6bad5d1edcfe150d3a867b72568ba6dcf8efc
Xiaofan <ufa.fan@gmail.com>
2016-01-26 17:48:08 -0800

    fix SettingsActivity up navigation problem(navigates to a new MainActivity where no item is selected)

--- app/src/main/java/com/example/xfang/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/xfang/sunshine/SettingsActivity.java
@@ -264,4 +264,10 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             return super.onOptionsItemSelected(item);
         }
     }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
 }
