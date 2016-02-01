a35518d1ef95e7ed049e983ba87eb7d9c5c144be
Xiaofan <ufa.fan@gmail.com>
2016-02-01 15:12:03 -0800

    hide fab for now

--- app/src/main/java/com/example/xfang/sunshine/MainActivity.java
+++ app/src/main/java/com/example/xfang/sunshine/MainActivity.java
@@ -48,15 +48,15 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
 
-        // fab
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        // fab
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         // locationSetting
         mLocation = Utilities.getPreferredLocationSetting(this);
