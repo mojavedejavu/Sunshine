e481c4ecdcf03856b84288e43326e51436f8dfa4
Xiaofan <ufa.fan@gmail.com>
2016-02-29 15:07:12 -0800

    Remove action bar; add toolbar style

--- app/src/main/java/com/example/xfang/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/xfang/sunshine/DetailActivity.java
@@ -27,20 +27,6 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.weather_detail_container, df)
                     .commit();
         }
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
     }
 
     @Override
--- app/src/main/java/com/example/xfang/sunshine/MainActivity.java
+++ app/src/main/java/com/example/xfang/sunshine/MainActivity.java
@@ -48,21 +48,6 @@ public class MainActivity extends AppCompatActivity
                 findFragmentById(R.id.forecast_fragment);
         ff.setUseTodayLayout(!mTwoPane);
 
-        // toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
-
-//        // fab
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         // locationSetting
         mLocation = Utilities.getPreferredLocationSetting(this);
 
